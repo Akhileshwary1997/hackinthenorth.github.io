cb63ee99cccf348d8a85628514cc5f504fb004a5
Akhileshwary Pandey <akhileshwary.pandey@gmail.com>
2018-03-17 14:46:35 +0530

    Add files via upload

---
+++ pehel/papp/urls.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    #url(r'^$', views.home, name='home'),
+    #url(r'check/$', views.check, name='check'),
+        
+    
+    
+        
+
+]
